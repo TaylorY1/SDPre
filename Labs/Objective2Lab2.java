33b1bf3f6f0d639f2bc7aac67710714a97c025d2
Taylor Y <yockeyt139@gmail.com>
2022-01-21 18:33:40 -0700

    Complete O2L2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Taylor Yockey";
+    int age = 31;
+    String state = "Iowa";
+      System.out.println("Name:" + name);
+      System.out.println("Age:" + age);
+      System.out.println("State:" + state);
+
+  }
+}
