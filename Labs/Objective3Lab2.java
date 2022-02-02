b77737327c3907df5d8808c3e923b5ddf5c9483c
Taylor Y <yockeyt139@gmail.com>
2022-02-01 17:47:21 -0700

    Complete O3L2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+ public static void main(String[] args){
+   double side1 = 6.0;
+    double side2 = 8.0;
+      double hypotenuse = Math.sqrt(side1 * side1 + side2 * side2);
+
+    System.out.print("The hypotenuse of a triangle with sides " + side1 +
+       " and " + side2 + " is ");
+        System.out.print(hypotenuse);
+      }
+}
