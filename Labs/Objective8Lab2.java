5d8156d7a906b0a4803df1f930bd059f648511e8
Taylor Y <yockeyt139@gmail.com>
2022-02-03 14:47:14 -0700

    Complete O8L2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,15 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sum = 0;
+
+    for (int G=1; G<=20; G+=1){
+      sum += G;
+    }
+      System.out.println(sum);
+
+
+
+}
+}  //TODO Write a for loop that will print the numbers 1-10
+
+    //TODO Write another for loop that will print the numbers 10-1.
