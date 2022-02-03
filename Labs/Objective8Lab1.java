2f6f3fd9d9aab3ac3bf9ad8cdaa43deba362e9ee
Taylor Y <yockeyt139@gmail.com>
2022-02-03 14:38:08 -0700

    Complete O8L1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,17 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int G=1; G<=10; G+=1){
+      System.out.println(G);
+}
+
+    for (int F=10; F>=1; F= F - 1){
+      System.out.println(F);
+}
+    //TODO Write a for loop that will print the numbers 1-10
+
+    //TODO Write another for loop that will print the numbers 10-1.
+
+
+  }
+}
