9e694f1c23d7bb59d3ff3c7a98cb1c24042bc95d
Taylor Y <yockeyt139@gmail.com>
2022-02-03 15:09:26 -0700

    Complete O8L3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,19 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    for (int G=1; G<=20; G+=1){
+      counter = G;
+
+    if (counter % 2 == 0) {
+      System.out.println(counter + " is even.");
+}
+    else {
+      System.out.println(counter + " is odd.");
+
+      }
+    /*
+      Your solution goes here
+    */}
+  }
+}
